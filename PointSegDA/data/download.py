f9d336cb90b170f1cfc292051e6cc30ed7f9190a
Vikr-182 <vikrant.dewangan@research.iiit.ac.in>
2021-10-05 17:06:48 +0530

    added files

--- PointSegDA/data/download.py
+++ PointSegDA/data/download.py
@@ -5,5 +5,5 @@ url = 'https://drive.google.com/uc?id=1dkU-8Y8K7yZaZjwelVUsxAbBf7JmOX9j'
 output = 'PointSegDAdataset.rar'
 gdown.download(url, output, quiet=False)
 
-os.system('unrar x PointSegDAdataset.rar')
-os.system('rm PointSegDAdataset.rar')
+#os.system('unrar x PointSegDAdataset.rar')
+#os.system('rm PointSegDAdataset.rar')
